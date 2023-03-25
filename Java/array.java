a95ff36a8ce670f10133b01902c54c6bbfcee044
Qi Da(Mike) Chen <qidachen808@yahoo.com>
2023-03-25 16:06:26 -0400

    added comments to array.java

--- Java/array.java
+++ Java/array.java
@@ -1,27 +1,33 @@
 package Java;
-import java.util.*;
+import java.util.*; // import the scanner function.
 
 public class array {
     public static void main(String[] arg){
-        Scanner scan = new Scanner(System.in);
-        double number[];
-        int size;
+        Scanner scan = new Scanner(System.in); // create a scanner called scan 
+        double number[]; // set array vaule as double so we can do decimal
+        int size; // size as int.
 
-        System.out.print("How many numbers would you like to put in the array: ");
-        size = scan.nextInt();
-        number = new double[size];
+        // Display message asking how much numbers you want in array
+        System.out.print("How many numbers would you like to put in the array: "); 
+        size = scan.nextInt(); // input the amount of number
+        number = new double[size]; // set the length of the number array to size.
 
+        // for loop for entering the numbers that is going to be store in your array.
         for(int x = 0; x < size; x++){
             System.out.print("Enter number " + (x+1) + ": ");
-            number[x] = scan.nextDouble();
+            number[x] = scan.nextDouble(); // x will increment each loop in the array 
+                                           // so the number will not write over last array index.
         }
 
+        // Display the number in the array.
         System.out.print("Here are the numbers in the array: ");
-        System.out.print(number[0]);
+        System.out.print(number[0]); 
+        // for loop to display the rest of the number.
         for(int x = 1; x < size; x++){
             System.out.print(", " + number[x]);
         }
 
+        // close the scanner because we dont need it anymore.
         scan.close();
     }
 }
