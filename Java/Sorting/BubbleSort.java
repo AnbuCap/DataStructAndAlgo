f18f73b76e08b1e2de2d7cd67656c63c12d88a04
Qi Da Chen (Mike) <83503846+AnbuCap@users.noreply.github.com>
2023-04-02 00:20:25 -0400

    now you can pick if you want to display the process of the bubble sort

--- Java/Sorting/BubbleSort.java
+++ Java/Sorting/BubbleSort.java
@@ -1,13 +1,21 @@
 // Bubble Sort
 // An algorithm that compares the current number to the next number in the array. If lower it will swap
 // Array = {20, 12, 32, 22, 6}
+import java.util.*;
 
 public class BubbleSort {
     public static void main(String[] arg){
-        int array[] = {20, 12, 32, 22, 6};
+        Scanner scan = new Scanner(System.in);       
+        int choice, array[] = {20, 12, 32, 22, 6};
+        do{
+            System.out.println("Want to display the progress of the bubble sort? (1)Yes (2) No: ");
+            choice = scan.nextInt();
+        }while(choice != 1 && choice != 2);
+        
         printUnsorted(array);
-        bubbleSort(array);
+        bubbleSort(array, choice);
         printSorted(array);
+        scan.close();
     }
 
     static void printUnsorted(int array[]){
@@ -23,22 +31,26 @@ public class BubbleSort {
     }
 
 
-    static void bubbleSort(int array[]){
+    static void bubbleSort(int array[], int choice){
         int temp = 0, progressPrinted = 0, swapAmount = 0;
         for(int x = 0; x < array.length; x++) {
             for(int y = 0; y < array.length - 1; y++){
                 if(array[y] > array[y + 1]){
-                    System.out.println(array[y] + " is greater than " + array[y+1] + "(swap)");
+                    if(choice == 1){
+                        System.out.println(array[y] + " is greater than " + array[y+1] + "(swap)");
+                    }
                      temp = array[y];
                      array[y] = array[y + 1];
                      array[y + 1] = temp;
                      swapAmount++;
                 }
-                else{
+                if(choice == 1){
                     System.out.println(array[y] + " and " + array[y+1] + " appears to be in the right order");
                 }
                 progressPrinted++;
-                printProgress(array, swapAmount, progressPrinted);
+                if(choice == 1){
+                    printProgress(array, swapAmount, progressPrinted);
+                }
             }
         }
     }
